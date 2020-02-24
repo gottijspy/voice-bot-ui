3edc85ce9d2d2fb2c289bc9c121588f06e629a87
Pranith Gottigundala <gottijspy@gmail.com>
2020-02-24 14:41:33 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,170 @@
+import React from "react";
+import Artyom from "artyom.js";
+import "./App.css";
+import microphone from "./microphone.svg";
+import ArtyomCommandsManager from "./ArtyomCommands.js";
+
+const Arya = new Artyom();
+
+class App extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.transcriptCount = 0;
+
+    // Add `this` context to the handler functions
+    this.startAssistant = this.startAssistant.bind(this);
+    this.stopAssistant = this.stopAssistant.bind(this);
+    this.speakText = this.speakText.bind(this);
+    this.handleTextareaChange = this.handleTextareaChange.bind(this);
+
+    this.state = {
+      pulse: false,
+      micStatus: "",
+      micText: "tap on microphone to get started...",
+      transcripts: []
+    };
+
+    // Load some commands to Artyom using the commands manager
+    let CommandsManager = new ArtyomCommandsManager(Arya);
+    CommandsManager.loadCommands(this.speakText);
+  }
+
+  speakText(msg, speak = true) {
+    let _this = this;
+
+    if (msg === "stopAssistant") {
+      _this.pulsate();
+      return;
+    }
+
+    _this.transcriptCount = _this.transcriptCount + 1;
+    _this.setState({
+      transcripts: _this.state.transcripts.concat({
+        id: _this.transcriptCount,
+        who: "Arya",
+        text: msg
+      })
+    });
+
+    if (speak) {
+      _this.setState({ micText: "speaking..." });
+      Arya.say(msg, {
+        onEnd: function() {
+          _this.setState({ micText: "listening..." });
+        }
+      });
+    }
+  }
+
+  startAssistant() {
+    let _this = this;
+
+    console.log("Artyom succesfully started !");
+
+    Arya.initialize({
+      lang: "en-GB",
+      debug: true,
+      continuous: true,
+      soundex: true,
+      listen: true
+    })
+      .then(() => {
+        _this.setState({ micText: "listening..." });
+      })
+      .catch(err => {
+        console.error("Oopsy daisy, this shouldn't happen !", err);
+      });
+
+    Arya.redirectRecognizedTextOutput(function(text, isFinal) {
+      _this.setState({ micText: text });
+      if (isFinal) {
+        _this.transcriptCount = _this.transcriptCount + 1;
+        _this.setState({
+          transcripts: _this.state.transcripts.concat({
+            id: _this.transcriptCount,
+            who: "You",
+            text: text
+          })
+        });
+
+        _this.setState({ micText: "listening..." });
+      }
+    });
+
+    // Add the event listener
+    Arya.when("NOT_COMMAND_MATCHED", () => {
+      _this.speakText(
+        "I'm sorry, I can't answer that! contacting DeepSearch, please wait..."
+      );
+    });
+  }
+
+  stopAssistant() {
+    let _this = this;
+
+    Arya.fatality()
+      .then(() => {
+        console.log("Arya has been succesfully stopped");
+        _this.setState({ micText: "tap on microphone to get started..." });
+      })
+      .catch(err => {
+        console.error("Oopsy daisy, this shouldn't happen neither!", err);
+
+        _this.setState({
+          artyomActive: false
+        });
+      });
+  }
+
+  handleTextareaChange(event) {
+    this.setState({
+      textareaValue: event.target.value
+    });
+  }
+
+  pulsate() {
+    this.setState({ pulse: !this.state.pulse }, () => {
+      if (this.state.pulse) {
+        this.startAssistant();
+      } else {
+        this.stopAssistant();
+      }
+    });
+  }
+
+  render() {
+    let btn_class = this.state.pulse ? "pulse pulse-active" : "pulse";
+    let dialog_class = this.state.pulse ? "transcript-show" : "transcript-hide";
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          {/* Microphone */}
+          <button onClick={this.pulsate.bind(this)} className={btn_class}>
+            <img src={microphone} className="App-logo" alt="logo" />
+          </button>
+
+          {/* recognition in progress */}
+          <p className="micStatus anim-typewriter">{this.state.micText}</p>
+
+          {/* Speech Transcripts */}
+          <div className={dialog_class}>
+            {this.state.transcripts.map(transcript => (
+              <p
+                className={
+                  transcript.who === "Arya" ? "dialog-said" : "dialog-heard"
+                }
+                key={transcript.id}
+              >
+                {transcript.who + ": " + transcript.text}
+              </p>
+            ))}
+          </div>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/ArtyomCommands.js
@@ -0,0 +1,112 @@
+// ArtyomCommands.js
+export default class ArtyomCommandsManager {
+  // The ArtyomCommandsManager class expects as argument in the constructor
+  // an already declared instance of Artyom.js
+  constructor(ArtyomInstance) {
+    this._artyom = ArtyomInstance;
+  }
+
+  // Execute the loadCommands method to inject the methods to the instance of Artyom
+  loadCommands(callback) {
+    let Artyom = this._artyom;
+
+    // Here you can load all the commands that you want to Artyom
+    return Artyom.addCommands([
+      {
+        indexes: ["Hello", "Hi", "Hey"],
+        action: () => {
+          Artyom.newPrompt({
+            question: "Hello there! May I have your name please?",
+            smart: true, //We set the smart property to true to accept wildcards
+            options: ["My name is  *", "This is *", "* here"],
+            onEndPrompt: () => {
+              callback("Hello there! May I have your name please?", false);
+            },
+            onMatch: (i, wildcard) => {
+              // i returns the index of the given options
+              var action;
+              var name = wildcard.charAt(0).toUpperCase() + wildcard.slice(1);
+              action = () => {
+                callback("Welcome, " + name);
+              };
+
+              // A function needs to be returned in onMatch event
+              // in order to accomplish what you want to execute
+              return action;
+            }
+          });
+          //callback("Hello there! May I have your name please?");
+        }
+      },
+      {
+        indexes: ["Fio Labs", "What does FIO Labs do?"],
+        action: () => {
+          callback(
+            "Fio Labs works on Data Science and Data Governance projects, to know more visit www.fiolabs.ai"
+          );
+        }
+      },
+      {
+        indexes: ["what's your name", "your name"],
+        action: () => {
+          callback("My name is Arya, what's yours?");
+        }
+      },
+      {
+        indexes: ["Bye", "Stop", "Good Bye", "Goodbye"],
+        action: () => {
+          callback("stopAssistant");
+        }
+      },
+      {
+        indexes: ["What's the time now?", "time now"],
+        action: () => {
+          var today = new Date();
+          var time =
+            today.getHours() +
+            ":" +
+            today.getMinutes() +
+            ":" +
+            today.getSeconds();
+          callback(time);
+        }
+      },
+      {
+        indexes: ["What's the time in US East Coast?", "US East Coast time"],
+        action: () => {
+          var usaToday = new Date().toLocaleString("en-US", {
+            timeZone: "America/New_York"
+          });
+          var usaTime =
+            usaToday.getHours() +
+            ":" +
+            usaToday.getMinutes() +
+            ":" +
+            usaToday.getSeconds();
+          callback(usaTime);
+        }
+      },
+      {
+        indexes: [/How are you/, /Regular expressions supported/],
+        smart: true,
+        action: () => {
+          callback("I'm fine, thanks for asking !");
+        }
+      },
+
+      {
+        indexes: ["Generate reports of * of this year"],
+        smart: true,
+        action: (i, month) => {
+          let year = new Date().getFullYear();
+
+          callback(`Generating reports of ${month} ${year} `);
+
+          callback(
+            "Ready ! What were you expecting? write some code you lazy bear !"
+          );
+        }
+      }
+    ]);
+  }
+}
